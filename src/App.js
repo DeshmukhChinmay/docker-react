ce34f3e8074c7e4d9ddc6b9f6ac01b209e995f96
DeshmukhChinmay <chinmay2207@gmail.com>
2021-04-14 11:54:47 +1200

    Changed index page button text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Bro!!!
+          This button's text was changed
         </a>
       </header>
     </div>
